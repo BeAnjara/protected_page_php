aed736ebdda7b84911b7084df4a8f25b6d5082e8
BeAnjara <beanjarahoussen@gmail.com>
2019-12-15 18:02:21 +0300

    Home page for the app

---
+++ index.php
@@ -0,0 +1,14 @@
+ <?php 
+ 	require 'header.php';
+ ?>
+ 
+<h1>Welcome the Nasa website</h1>
+
+ 
+
+<p> Click <a href="./form.php"> here </a> to navigate in the secret room</p>
+
+
+<?php 
+	require 'footer.php';
+?>
